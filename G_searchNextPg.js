b5482842377bc702fdafc67a448d933d3e4aa1af
andrebradshaw <bradshaw.andre@gmail.com>
2017-06-06 11:28:14 -0400

    Create G_searchNextPg.js

---
+++ G_searchNextPg.js
@@ -0,0 +1,15 @@
+var first = '&start=10';
+var addon = '&start=';
+var regPrime = /&start=(\d+)/g;
+var url = window.location.href;
+var str;
+while ((str = regPrime.exec(url)) !== null) {
+	var numInt = parseInt(str[1]);
+	var next = numInt+10;
+}
+	if (next == null) {
+	var	newURL = window.location.href + first;
+	} else {
+	var newURL = url.replace(/&start=\d+/g, addon + next);
+	}
+window.location.href = newURL;
